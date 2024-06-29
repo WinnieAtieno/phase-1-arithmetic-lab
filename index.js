165465fd9d65fd1c0cb3596cd0f2462c76edacd3
WinnieAtieno <winnieotieno773@gmail.com>
2024-06-29 11:52:35 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
+let num1 = 2;
+let num2 = 31;
+let num3 = 5;
+let num4 = 29;
+let numbers = [20,3,16, 9,7]
 // Write your code here
+let multiply= num1 * num2;
+
+let random = Math.floor(Math.random() * 10 +1 );
+
+let mod = num4 % num3;
+ 
+let max = Math.max(...numbers)
+ console.log(multipy)
+ console.log(random)
+ console.log(mod)
+ console.log(max)
